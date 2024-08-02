b77ace6f096a52cac7fded127570efada52a9ba8
Ravi62026 <rs1806105@gmail.com>
2024-08-02 14:27:48 +0530

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,6 +10,14 @@ module.exports = {
     sepolia :{
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    amoy : {
+      url: `https://polygon-amoy.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    fantomtest : {
+      url: process.env.PROVIDER_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
     }
   },
   settings:{
